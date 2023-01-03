0564c911e6b16940d3873c7cf6b55f570b098904
liviapz <liviapavinizeviani@gmail.com>
2023-01-03 20:30:23 -0300

    comment some lines that are nomore necessary, because of the constructor

--- src/Meeting.java
+++ src/Meeting.java
@@ -1,11 +1,11 @@
 public class Meeting {
 
     public static void main(String[] args) {
-        Date meetingDate = new Date(2, 29, 2023);
-
-        meetingDate.month = 2;
-        meetingDate.day = 29;
-        meetingDate.year = 2023;
+        Date meetingDate = new Date(29, 2, 2023);
+// this is not necessary anymore
+//        meetingDate.month = 2;
+//        meetingDate.day = 29;
+//        meetingDate.year = 2023;
 
         System.out.println("The meeting will be on " + meetingDate.month + "/" + meetingDate.day + "/" + meetingDate.year);
     }
