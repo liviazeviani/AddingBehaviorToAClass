116d3a6ee31c5fe201fcd46a50f6cfab0e26e66d
liviapz <liviapavinizeviani@gmail.com>
2023-01-04 01:08:42 -0300

    a new method to determinate if is needed to increment the day or the month. in this method we need to call another method that will tell us how many days there are in each month

--- src/Date.java
+++ src/Date.java
@@ -4,6 +4,17 @@ public class Date {
     public int month;
     public int year;
 
+   
+    public void nextDay(){
+        int dayCount = daysInMonth(this.month, this.year);
+        this.day++;
+        if (this.day > dayCount){
+            this.day = 1;
+            this.month++;
+        }
+
+    }
+
     public Date(int d, int m, int y){
         this.day = d;
         this.month = m;
